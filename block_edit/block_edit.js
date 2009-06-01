9109eeaa5ec32176c5ac272223e47e00375202f6
Abraham Williams <4braham@gmail.com>
2009-06-01 03:05:13 -0500

    updated vertical_tabs, date, captcha, block_edit, admin_menu and addthis

--- block_edit/block_edit.js
+++ block_edit/block_edit.js
@@ -1,43 +1,33 @@
-/*
-  Copyright (C) 2008 by Phase2 Technology.
-  Author: Irakli Nadareishvili
+// $Id: block_edit.js,v 1.1.2.6 2009/05/26 21:24:02 psynaptic Exp $
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License.
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY. See the LICENSE.txt file for more details.
+$(document).ready(function() {
 
- */
-
-$(document).ready(function(){
-
-    var regexp = new RegExp(/block-(.+?)-(.+?)/mi);
-    var checkp = new RegExp(/block-.*?-.+/mi);
+  var regexp = new RegExp(/block-(.+?)-(.+?)/mi);
+  var checkp = new RegExp(/block-.*?-.+/mi);
 
   $("div.block").each(function (i) {
     var block_id = $(this).attr('id');
-  if (block_id.match(checkp)) {
+    if (block_id.match(checkp)) {
       var block_path = block_id.replace(regexp, '$1/$2');
       block_id = block_id.replace(regexp, '$1_$2');
-    var block_link = '<div id="bel_id_' + block_id + '" class="block_edit_link"><a href="/admin/build/block/configure/' + block_path + '">Edit</a></div>';
+      var block_link = '<div id="block-edit-link-' + block_id + '" class="block-edit-link"><a href="' + Drupal.settings.basePath + 'admin/build/block/configure/' + block_path + '?' + Drupal.settings.block_edit.destination + '">[Configure]</a></div>';
       $(this).prepend(block_link);
     }
   });
-  
-  $("div.block").mouseover(function(){
+
+  $("div.block").mouseover(function() {
     var block_id = $(this).attr('id');
     if (block_id.match(checkp)) {
       block_id = block_id.replace(regexp, '$1_$2');
-      $('div#bel_id_'+block_id).css('display', 'block');
+      $('div#block-edit-link-' + block_id).css('display', 'block');
     }
   });
 
-  $("div.block").mouseout(function(){
-    var block_id = $(this).attr('id'); 
+  $("div.block").mouseout(function() {
+    var block_id = $(this).attr('id');
     if (block_id.match(checkp)) {
       block_id = block_id.replace(regexp, '$1_$2');
-      $('div#bel_id_'+block_id).css('display', 'none');
+      $('div#block-edit-link-' + block_id).css('display', 'none');
     }
   });
-  
 });
